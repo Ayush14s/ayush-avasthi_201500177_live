844f6b1c6d7b67c7866f055f048fa24f4c209752
Ayush <ayush.avasthi_cs20@gla.ac.in>
2022-07-11 20:52:08 +0530

    my templet

---
+++ script.js
@@ -0,0 +1,11 @@
+var today = new Date();
+  var day = today.getDay();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var date = today.getDate()+'-'+ (today.getMonth()+1)+'-'+today.getFullYear();
+  document.getElementById("date").innerHTML = date;
+ var name = 'Ayush Avasthi';
+ document.getElementById('name').innerHTML=name;
+ var roll = '201500177';
+ document.getElementById('roll').innerHTML=roll;
+ var university = 'GLA University';
+ document.getElementById('university').innerHTML=university;
